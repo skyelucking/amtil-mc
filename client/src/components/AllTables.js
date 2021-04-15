e00324114641fa12b6131ce5488b1ffeafa0f1db
Skye Lucking <lucking.skye@gmail.com>
2021-04-14 18:15:16 -0700

    changed start script to go into the server folder

--- client/src/components/AllTables.js
+++ client/src/components/AllTables.js
@@ -7,13 +7,13 @@ import ShowEquip from "./ShowEquip";
 function AllTables() {
   return (
    <div>
-      <div style={{ textAlign: "center", color: "red" }}>
+      <div style={{ textAlign: "center", color: "red", margin: 15  }}>
           <b>Elements that Go Into A Mission (Feeds Select and Input Menus)</b>
         </div>
     
        <ShowEquip/>
   
-       <div style={{ textAlign: "center", color: "red" }}>
+       <div style={{ textAlign: "center", color: "red", margin: 15 }}>
           <b>Mission Specific Items (Tied by the mission_id param)</b>
         </div>
    <ShowBasics />
